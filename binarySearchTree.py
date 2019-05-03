080702a834289388c3bba5952db77836c128c24b
Anatoliy Bezhenaru <bejena-ru@ya.ru>
2019-05-04 02:35:07 +0700

    initial commit

---
+++ binarySearchTree.py
@@ -0,0 +1,130 @@
+class BSTNode:
+
+    def __init__(self, key, val, parent):
+        self.NodeKey = key  # ключ узла
+        self.NodeValue = val  # значение в узле
+        self.Parent = parent  # родитель или None для корня
+        self.LeftChild = None  # левый потомок
+        self.RightChild = None  # правый потомок
+
+
+class BSTFind:  # промежуточный результат поиска
+
+    def __init__(self):
+        self.Node = None  # None если не найден узел
+        self.NodeHasKey = False  # True если узел найден
+        self.ToLeft = False  # True, если родительскому узлу надо
+        # добавить новый узел левым потомком
+
+
+class BST:
+
+    def __init__(self, node):
+        self.Root = node  # корень дерева, или None
+
+    def FindNodeByKey(self, key):
+        node = self.Root
+        bsTFind = BSTFind()
+
+        if key == node.NodeKey:
+            bsTFind.Node = node
+            bsTFind.NodeHasKey = True
+            bsTFind.ToLeft = False
+
+        elif key > node.NodeKey and node.RightChild is None:
+            bsTFind.Node = node
+            bsTFind.NodeHasKey = False
+            bsTFind.ToLeft = False
+
+        elif key < node.NodeKey and node.LeftChild is None:
+            bsTFind.Node = node
+            bsTFind.NodeHasKey = False
+            bsTFind.ToLeft = True
+
+        elif key < node.NodeKey:
+            node = BST(node.LeftChild)
+            return node.FindNodeByKey(key)
+
+        elif key > node.NodeKey:
+            node = BST(node.RightChild)
+            return node.FindNodeByKey(key)
+
+        return bsTFind
+
+    def AddKeyValue(self, key, val):
+        node = self.FindNodeByKey(key)
+
+        if node.NodeHasKey is False:
+
+            if node.ToLeft == True:
+                node.Node.LeftChild = BSTNode(key, val, node.Node)
+
+            else:
+                node.Node.RightChild = BSTNode(key, val, node.Node)
+
+            return True
+
+        else:
+            return False
+
+
+    def FinMinMax(self, FromNode, Findmax):
+        findNode = self.FindNodeByKey(FromNode)
+        node = findNode.Node
+
+        if Findmax is True:
+            if node.RightChild is None:
+                return node.NodeKey
+
+            else:
+                node = BST(node.RightChild)
+                return node.FinMinMax(node.Root.NodeKey, True)
+
+        if Findmax is False:
+            if node.LeftChild is None:
+                return node.NodeKey
+
+            else:
+                node = BST(node.LeftChild)
+                return node.FinMinMax(node.Root.NodeKey, False)
+
+    def DeleteNodeByKey(self, key):
+        deleteNode = self.FindNodeByKey(key)
+        left = deleteNode.Node.LeftChild
+        right = deleteNode.Node.RightChild
+
+        if left and right:
+            replaceKey = self.FinMinMax(right.NodeKey, False)
+            replaceNode = self.FindNodeByKey(replaceKey)
+
+            if replaceNode.Node.RightChild:
+                replaceNode = replaceNode.Node.RightChild
+
+            else:
+                replaceNode = replaceNode.Node
+
+        elif left and right is None:
+            replaceNode = left
+
+        else:
+            replaceNode = right
+
+        deleteNode.Node.NodeValue = replaceNode.NodeValue
+        deleteNode.Node.NodeKey = replaceNode.NodeKey
+        replaceNode.Parent.RightChild = None
+        replaceNode.Parent.LeftChild = None
+
+
+    def Count(self, count = []):
+        node = self.Root
+        count.append(node.NodeKey)
+
+        if node.LeftChild:
+            lnode = BST(node.LeftChild)
+            lnode.Count()
+
+        if node.RightChild:
+            rnode = BST(node.RightChild)
+            rnode.Count()
+
+        return count
---
+++ binarySearchTreeTest.py
@@ -0,0 +1,65 @@
+
+
+node_0 = BSTNode(0, 0, None)
+node_1 = BSTNode(1, 1, None)
+node_3 = BSTNode(3, 3, None)
+node_5 = BSTNode(5, 5, None)
+node_7 = BSTNode(7, 7, None)
+
+node_9 = BSTNode(9, 9, None)
+node_11 = BSTNode(11, 11, None)
+node_13 = BSTNode(13, 13, None)
+node_15 = BSTNode(15, 15, None)
+
+
+
+node_8 = BSTNode(8, 8, None)
+node_4 = BSTNode(4, 4, node_8)
+node_12 = BSTNode(12, 12, node_8)
+
+node_2 = BSTNode(2, 2, node_4)
+node_6 = BSTNode(6, 6, node_4)
+
+
+node_10 = BSTNode(10, 10, node_12)
+node_14 = BSTNode(14, 14, node_12)
+
+
+
+tree = BST(node_8)
+
+node_8.LeftChild = node_4
+node_8.RightChild = node_12
+node_4.LeftChild = node_2
+node_4.RightChild = node_6
+node_12.LeftChild = node_10
+node_12.RightChild = node_14
+
+
+print(tree.AddKeyValue(1, 1))
+print(tree.AddKeyValue(11, 11))
+print(tree.AddKeyValue(13, 13))
+# print(tree.AddKeyValue(6, 6))
+
+# print(tree.AddKeyValue(5, 5))
+print(tree.AddKeyValue(7, 7))
+print(tree.AddKeyValue(15, 15))
+print(tree.AddKeyValue(3, 3))
+# print(tree.AddKeyValue(5, 5))
+# print(tree.AddKeyValue(9, 9))
+# # print()
+# print(tree.FindNodeByKey(9).Node.Parent.NodeKey)
+
+# print("min tree", tree.FinMinMax(10, False))
+
+# print(node_4.RightChild.NodeValue)
+
+# print(tree.Count())
+
+# print(tree.Root.RightChild.RightChild.LeftChild.NodeKey)
+
+# nd = tree.FindNodeByKey(3)
+tree.DeleteNodeByKey(12)
+# print(nd.Node.NodeValue)
+
+print(tree.Count())
